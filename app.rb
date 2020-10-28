d29098bb63aa570ac72c070e78cb9c44a31e12ac
giegeralexandra <giegeralexandra@gmail.com>
2020-10-28 19:39:15 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do  
+        @session = session
+        @session[:item]= params[:item]
+        erb :checkout
+    end
+
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,12 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure do 
+    enable :sessions
+    set :session_secret, "secret"
+end
+
 require './app'
 require_all 'models'
+
+
